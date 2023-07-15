83f577de9184ac92299e2a99dbf33adbb8880239
ugly-bastard <himanshu.mishra1123@gmail.com>
2023-07-15 23:15:14 +0530

    docker with persistent data, comments on api CRUD operations

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import express from "express";
 import db from "./db.js";
 import morgan from "morgan";
 
-// const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 const app = express();
 
 app.use(morgan('dev'))
@@ -14,38 +14,65 @@ app.use(express.urlencoded({ extended: true }))
 let count = 0;
 
 app.get("/", (req, res) => {
-    count++;
-    res.send(`hello  world, this site has been visited ${count} times`)
+  count++;
+  res.send({
+    hello: "Hello world!",
+    visits: `This site has been visited ${count} times!`
+  })
 })
 
-//ROUTES
+// ROUTES
 
-//CREATE A TODO
-app.post("/todos", async(req,res)=>{
-    try{
-        const newTodo = await db('todos').insert({ description: req.body.description }).returning('*')
-        res.json(newTodo)
-        // const { description } = req.body;
-        // const newTodo = await db.query(
-        //     "INSERT INTO todo {description} VALUES($1)",
-        //     [description]
-        // );
-        // res.json(newTodo);
-    }catch(err){
-        console.error(err.message)
-    }
-})
-
-//DELETE A TODO
-
-//UPDATE A TODO
-
-//GET ALL TODOS
+// GET ALL TODOS
 app.get('/todos', async (req, res) => {
   const todo = await db.select().from('todos')
   res.json(todo)
 })
 
-//GET A TODO
+// // GET ONE TODO
+// app.get('/todos/:id', async (req, res) => {
+//   // TEST THIS req.params.id STUFF
+//   const id = req.params.id
+//   const todo = await db.select(id).from('todos')
+//   res.json(todo)
+// })
 
-app.listen(5000, () => console.log(`server running on port 5000`))
+// CREATE A TODO
+app.post("/todos", async(req,res)=>{
+  try{
+    const newTodo = await db('todos').insert({ description: req.body.description }).returning('*')
+    res.status(201).json(newTodo)
+    // const { description } = req.body;
+    // const newTodo = await db.query(
+    //     "INSERT INTO todo {description} VALUES($1)",
+    //     [description]
+    // );
+    // res.json(newTodo);
+  }catch(err){
+    res.status(400).json({ message: err.message })
+    console.error(err.message)
+  }
+})
+
+// // UPDATE A TODO
+// app.put('/todos/:id', async (req, res) => {
+//   const id = req.params.id
+// })
+//
+// // UPDATE A TODO ELEMENT
+// app.patch('/todos', async (req, res) => {
+//   const todo = await db.select().from('todos')
+//   res.json(todo)
+// })
+//
+// // DELETE A TODO
+// app.delete("/todos/:id", async(req,res) => {
+//   try {
+//     const id = req.params.id
+//     res.status(200)
+//   } catch (err) {
+//     console.error(err.message)
+//   }
+// })
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
