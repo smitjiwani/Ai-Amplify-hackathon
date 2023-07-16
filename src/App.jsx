32a495bb35e2620507fc26a029a8ceb2ebe9f88c
smit <smitjiwani44@gmail.com>
2023-07-17 05:04:09 +0530

    Turned js to jsx elements

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import InputTodo from './component/InputTodo'
+import Login from './component/Login'
+import Footer from './component/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <InputTodo />
+      App
     </>
   )
 }
