9e2b5a5625e3a224ad3a2db4001ae6167f0ba71e
smit <smitjiwani44@gmail.com>
2023-07-18 05:33:53 +0530

    input

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -8,25 +8,24 @@ function InputTodo() {
 
   const getTodo = async e => {
     e.preventDefault()
-    axios.get("http://localhost:5000/todos")
+    axios.get("http://localhost:5000/api/todos")
       .then(res => console.log(res))
       .catch(err => console.error(err))
   }
 
   const onSubmitForm = async e => {
     e.preventDefault()
-    axios.post("http://localhost:5000/todos", {description})
+    axios.post("http://localhost:5000/api/todos", {description})
       .then(res => console.log(res))
       .catch(err => console.error(err))
   }
 
-    // const delTodo = async e => {
+  // const delTodo = async e => {
   //   e.preventDefault()
   //   axios.delete("http://localhost:5000/api/todos/:id")
   //     .then(res => console.log(res))
   //     .catch(err => console.error(err))
   // }
-
 
   return (
     <div>
